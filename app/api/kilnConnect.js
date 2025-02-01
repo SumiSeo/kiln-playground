3c7e88972b1f8355a07c09c8b775b9a84616c659
Sumi <qkobr94@gmail.com>
2025-02-01 21:58:35 +0100

    feat/display-in-table

--- app/api/kilnConnect.js
+++ app/api/kilnConnect.js
@@ -18,3 +18,24 @@ export const getDataVault1 = async () => {
     "/v1/defi/network-stats?vaults=matic_0x66431b90985212d3b09e27ff9b83cb32f6dd79dc"
   );
 };
+
+export const getDataVault2 = async () => {
+  return await k.client.GET(
+    "/v1/defi/network-stats?vaults=matic_0x290f5566a5269a52ad70d01ac860456b3b964f01"
+  );
+};
+export const getDataVault3 = async () => {
+  return await k.client.GET(
+    "/v1/defi/network-stats?vaults=matic_0x15dcc1978f68c5e0d7a298a65fcc879e2d673d43"
+  );
+};
+export const getDataVault4 = async () => {
+  return await k.client.GET(
+    "/v1/defi/network-stats?vaults=matic_0xab3ac228cac84a8a1c855c3e08f869b65836c962"
+  );
+};
+export const getDataVault5 = async () => {
+  return await k.client.GET(
+    "/v1/defi/network-stats?vaults=matic_0xdb8c962e8a39d3e82d3eaa8f477be90984c6dfe8"
+  );
+};
--- app/markets4626/page.tsx
+++ app/markets4626/page.tsx
@@ -1,19 +1,29 @@
 "use client";
 import { useEffect, useState } from "react";
-import { getDataVault1 } from "../api/kilnConnect";
+import { getDataVault1, getDataVault2 } from "../api/kilnConnect";
 import "../scss/Markets4626.scss";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import Paper from "@mui/material/Paper";
 
 export default function Markets4626() {
   const [data1, setData1] = useState();
-  const [rows, setRows] = useState();
+  const [rows, setRows] = useState([]);
   const columns: GridColDef[] = [
-    { field: "vaultSymbol", headerName: "vault", width: 100 },
-    { field: "vaultName", headerName: "vault name", width: 230 },
+    { field: "asset_symbol", headerName: "vault", width: 100 },
+    { field: "protocol_display_name", headerName: "vault name", width: 230 },
     {
-      field: "vaultPriceUSD",
+      field: "asset_price_usd",
       headerName: "vault price usd",
+      width: 230,
+    },
+    {
+      field: "chain",
+      headerName: "chain",
+      width: 230,
+    },
+    {
+      field: "updated_at_block",
+      headerName: "updated date",
       width: 230,
     },
   ];
@@ -21,11 +31,15 @@ export default function Markets4626() {
     const fetchData = async () => {
       try {
         const res = await getDataVault1();
+        const res2 = await getDataVault2();
         const rawData = res?.data;
+        const rawData2 = res2?.data;
         const { data } = rawData;
-        setData1(data[0]);
-        // setRows.push(data[0]);
-        console.log("data", data[0]);
+        if (data?.length > 0) {
+          const formattedData = { id: Date.now(), ...data[0] }; // Add a unique ID
+          setData1(formattedData);
+          setRows([{ id: Date.now(), ...data[0] }, ...rows]); // Ensure all rows have an ID
+        }
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -35,7 +49,7 @@ export default function Markets4626() {
   }, []);
   return (
     <div className="markets">
-      <div className="markets__title">Markets4626</div>
+      <div className="markets__title">Markets 4626</div>
       <div className="markets__table">
         <Paper sx={{ height: 400, width: "70%" }}>
           <DataGrid
