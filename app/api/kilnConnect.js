401b938bd696932121a8f38832a58d0ec361b41c
Sumi <qkobr94@gmail.com>
2025-02-01 17:16:03 +0100

    feat/rest-api-call

--- app/api/kilnConnect.js
+++ app/api/kilnConnect.js
@@ -1,12 +1,16 @@
 import { Kiln } from "@kilnfi/sdk";
+import "../scss/Markets4626.scss";
 
 const k = new Kiln({
   baseUrl: "https://api.kiln.fi",
   apiToken: "kiln_DBmNa8Y4Eu7O1ZCx9QMdTS4fQckBnWOEuwEqw9IM",
+  //testnet defi vault
+  //apiToken: "kiln_hnQNjKOhEhFfLOEcOUJrgrtc2LGcgqUj4a6MoyAt",
 });
+export const getData = async () => {
+  return await k.client.GET(
+    "/v1/defi/network-stats?vaults=matic_0x66431b90985212d3b09e27ff9b83cb32f6dd79dc"
+  );
+};
 
-const tx = await k.client.GET(
-  "/v1/defi/network-stats?vaults=matic_0x66431b90985212d3b09e27ff9b83cb32f6dd79dc"
-);
 
-console.log("tx ->", tx.data);
--- app/markets4626/page.tsx
+++ app/markets4626/page.tsx
@@ -1,6 +1,22 @@
+"use client";
+import { useEffect } from "react";
+import { getData } from "../api/kilnConnect";
+
 export default function Markets4626() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await getData();
+        console.log("res", res);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData(); // Call the async function
+  }, []); // Empty dependency array → runs once when component mounts;
   return (
-    <div>
+    <div className="markets">
       <div>Markets4626</div>
     </div>
   );
