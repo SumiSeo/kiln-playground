b99a8b33da85fe675e1a38550f50848b857cee33
Jay Moker <dadamien78@gmail.com>
2025-02-02 01:16:03 +0100

    Lets merge

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,40 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
+import Web3 from "web3";
+import ABI from "../kiln/ABI.json";
+
+const web3 = new Web3("http://localhost:8545");
+const contractAddress = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
+const contract = new web3.eth.Contract(ABI, contractAddress);
+
+// async function createAgreementAndLog() {
+//   try {
+//     const result = await contract.methods.createAgreement(
+//       "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
+//       "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+//       "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",
+//       1
+//     ).call();
+//     console.log(result);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+contract.methods.createAgreement("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
+								 "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+								 "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC", 1).call()
+									.then((result: number) => { console.log(result);})
+									.catch((error: any) => { console.log(error);});
+	// <div>
+	// 	<button onClick={async () => { await createAgreementAndLog();}}> test</button>
+	// </div>
+console.log(ABI);
+
 export default function Home() {
   return (
+
     <div className="home">
       <div className="home__box">available escroc contracts</div>
       <div className="home__box">
--- kiln/test/Escrow.t.sol
+++ kiln/test/Escrow.t.sol
@@ -9,14 +9,14 @@ contract CounterTest is Test {
 
     function setUp() public {
         escrow = new Escrow();
-        // escrow.createAgreement(0x0,0x01,0x11, 1);
+        escrow.createAgreement(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, 0x70997970C51812dc3A010C7d01b50e0d17dc79C8, 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC, 9);
     }
 
-    // function test_Increment() public {
-    //     escrow.increment();
-    //     assertEq(escrow.number(), 1);
+    // function test_Deposit() public {
+    //     escrow.deposit(0);
+    //     assertEq(escrow.agreements.length, 1);
     // }
-    //
+
     // function testFuzz_SetNumber(uint256 x) public {
     //     escrow.setNumber(x);
     //     assertEq(escrow.number(), x);
